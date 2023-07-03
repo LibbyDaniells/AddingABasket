30467ae0e8ed894b71b1b64d89a174c7bdb5da9e
LibbyDaniells <101347040+LibbyDaniells@users.noreply.github.com>
2023-07-03 11:23:21 +0100

    Update FindingDelta.R

--- GitHub-AddingABasket/CalibratingAcrossScenarios/Robustness To Sample Size/Worst Case 3b/FindingDelta.R
+++ GitHub-AddingABasket/CalibratingAcrossScenarios/Robustness To Sample Size/Worst Case 3b/FindingDelta.R
@@ -1,4 +1,6 @@
-#Cut-Offs across 1-10
+#Finding cut-off values for posterior probabilities under Method 3 for each sample size n5=1 up to 24 (size of existing baskets)
+
+#Calibrate cut-Offs across 1-10
 cut_off <- matrix(NA,nrow=24,ncol=5)
 for(i in 1:24){
   cut_off[i,1:4] <- colQuantiles(Method3Cal[[i]]$`Combined Cut-Offs`,probs=0.9,na.rm=T)[4]
@@ -7,14 +9,14 @@ for(i in 1:24){
 cut_off
 save(cut_off,file='WorstCaseDelta1_10.RData')
 
-
+#Plot cut-off values
 par(mfrow=c(1,2))
 plot(cut_off[,1],xaxt='n',ylab=expression(Delta[1]),xlab=expression(n[5]),main='Existing Baskets',pch=16,ylim=c(0.86,0.92))
 axis(1,seq(1,24,2))
 plot(cut_off[,5],xaxt='n',ylab=expression(Delta[5]),xlab=expression(n[5]),main='New Basket',pch=16,ylim=c(0.86,0.92))
 axis(1,seq(1,24,2))
 
-#Cut-Offs across 1-5
+#Calibrate cut-Offs across 1-5
 cut_off <- matrix(NA,nrow=24,ncol=5)
 for(i in 1:24){
   Comb <- rbind(Method3Cal[[i]]$`Scenario Cut-offs`[[1]],Method3Cal[[i]]$`Scenario Cut-offs`[[2]],Method3Cal[[i]]$`Scenario Cut-offs`[[3]],Method3Cal[[i]]$`Scenario Cut-offs`[[4]],Method3Cal[[i]]$`Scenario Cut-offs`[[5]])
@@ -23,6 +25,7 @@ for(i in 1:24){
 }
 save(cut_off,file='WorstCaseDelta1_5.RData')
 
+#Plot cut-off values
 par(mfrow=c(1,2))
 plot(cut_off[,1],xaxt='n',ylab=expression(Delta[1]),xlab=expression(n[5]),main='Existing Baskets',pch=16,ylim=c(0.86,0.92))
 axis(1,seq(1,24,2))
