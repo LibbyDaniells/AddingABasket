b42d219ce318d2eb17f4ea61a1c444df9f7f8789
LibbyDaniells <101347040+LibbyDaniells@users.noreply.github.com>
2023-07-03 11:10:32 +0100

    Update Method3Calibration.R

--- GitHub-AddingABasket/CalibratingAcrossScenarios/2+2 Baskets/Method3Calibration.R
+++ GitHub-AddingABasket/CalibratingAcrossScenarios/2+2 Baskets/Method3Calibration.R
@@ -1,3 +1,5 @@
+#Calibration of cut-off values under Method 3(a) where Delta is calibrated across scenarios
+
 library(rjags)
 library(textmineR)
 library(matrixStats)
@@ -57,6 +59,7 @@ q1 <- 0.4
 run <- 5000
 pw <- 0.3
 
+#Data scenarios considered to calibrate across
 p1 <- c(q0,q0,q0,q0)
 p2 <- c(q1,q0,q0,q0)
 p3 <- c(q1,q1,q0,q0)
